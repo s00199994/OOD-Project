06c366e99a008caf68880b1ddb4407d9ff73dca3
Keanan McDonald <keananmcdonald5@gmail.com>
2021-02-04 22:54:25 +0000

    Created Drivers class and made constructors

---
+++ OOD Project/Drivers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOD_Project
+{
+    class Drivers
+    {
+        public string Name { get; set; }
+        public int Podiums { get; set; }
+        public bool Champion { get; set; }
+
+        public Drivers(string name, int podiums, bool champion)
+        {
+            Name = name;
+            Podiums = podiums;
+            Champion = champion;
+        }
+
+        public Drivers()
+        {
+
+        }
+    }
+}
