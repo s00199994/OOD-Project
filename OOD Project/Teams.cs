89d380489dc37f48c7f4b83b59a758101a1784fe
Keanan McDonald <keananmcdonald5@gmail.com>
2021-02-04 11:46:33 +0000

    Add project files.

---
+++ OOD Project/Teams.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOD_Project
+{
+    class Teams
+    {
+        public string TeamName { get; set; }
+
+        public int YearFormed { get; set; }
+
+        public string Principal { get; set; }
+
+        public Teams(string teamName, int yearFormed, string principal)
+        {
+            TeamName = teamName;
+            YearFormed = yearFormed;
+            Principal = principal;
+        }
+
+        public override string ToString()
+        {
+            return TeamName;
+        }
+
+    }
+}
